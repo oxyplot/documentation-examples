a59a316198e602266bc1e7a7d2706bbf186bcf2b
Oystein Bjorke <oystein.bjorke@gmail.com>
2014-08-25 16:21:45 +0200

    Add WpfExamples project

---
+++ WpfExamples/NormalDistributionsWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿namespace WpfExamples
+{
+    using System;
+
+    using OxyPlot;
+    using OxyPlot.Axes;
+    using OxyPlot.Series;
+
+    /// <summary>
+    /// Interaction logic for NormalDistributionsWindow.xaml
+    /// </summary>
+    public partial class NormalDistributionsWindow
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NormalDistributionsWindow"/> class.
+        /// </summary>
+        public NormalDistributionsWindow()
+        {
+            this.InitializeComponent();
+            this.Model = CreateNormalDistributionModel();
+            this.DataContext = this;
+        }
+
+        /// <summary>
+        /// Gets or sets the model.
+        /// </summary>
+        /// <value>The model.</value>
+        public PlotModel Model { get; set; }
+
+        /// <summary>
+        /// Creates a model showing normal distributions.
+        /// </summary>
+        /// <returns>A PlotModel.</returns>
+        private static PlotModel CreateNormalDistributionModel()
+        {
+            // http://en.wikipedia.org/wiki/Normal_distribution
+            var plot = new PlotModel
+            {
+                Title = "Normal distribution",
+                Subtitle = "Probability density function"
+            };
+
+            plot.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Left,
+                Minimum = -0.05,
+                Maximum = 1.05,
+                MajorStep = 0.2,
+                MinorStep = 0.05,
+                TickStyle = TickStyle.Inside
+            });
+            plot.Axes.Add(new LinearAxis
+            {
+                Position = AxisPosition.Bottom,
+                Minimum = -5.25,
+                Maximum = 5.25,
+                MajorStep = 1,
+                MinorStep = 0.25,
+                TickStyle = TickStyle.Inside
+            });
+            plot.Series.Add(CreateNormalDistributionSeries(-5, 5, 0, 0.2));
+            plot.Series.Add(CreateNormalDistributionSeries(-5, 5, 0, 1));
+            plot.Series.Add(CreateNormalDistributionSeries(-5, 5, 0, 5));
+            plot.Series.Add(CreateNormalDistributionSeries(-5, 5, -2, 0.5));
+            return plot;
+        }
+
+        private static LineSeries CreateNormalDistributionSeries(double x0, double x1, double mean, double variance, int n = 1000)
+        {
+            var ls = new LineSeries
+            {
+                Title = string.Format("μ={0}, σ²={1}", mean, variance)
+            };
+
+            for (int i = 0; i < n; i++)
+            {
+                double x = x0 + ((x1 - x0) * i / (n - 1));
+                double f = 1.0 / Math.Sqrt(2 * Math.PI * variance) * Math.Exp(-(x - mean) * (x - mean) / 2 / variance);
+                ls.Points.Add(new DataPoint(x, f));
+            }
+
+            return ls;
+        }
+    }
+}
