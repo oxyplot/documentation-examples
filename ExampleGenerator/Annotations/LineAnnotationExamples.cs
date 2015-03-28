edea4d7ade64951eb063e38fec6138b89c336af6
Oystein Bjorke <oystein.bjorke@gmail.com>
2015-03-28 12:46:30 +0100

    ExampleGenerator: Add first annotation and axis examples

---
+++ ExampleGenerator/Annotations/LineAnnotationExamples.cs
@@ -0,0 +1,19 @@
+﻿namespace ExampleGenerator
+{
+    using OxyPlot;
+    using OxyPlot.Annotations;
+    using OxyPlot.Axes;
+
+    public class LineAnnotationExamples
+    {
+        [Export(@"Annotations\LineAnnotation")]
+        public static PlotModel LineAnnotation()
+        {
+            var model = new PlotModel { Title = "LineAnnotation" };
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left });
+            model.Annotations.Add(new LineAnnotation { X = 50, Type = LineAnnotationType.Vertical, Text = "Vertical LineAnnotation" });
+            return model;
+        }
+    }
+}
---
+++ ExampleGenerator/Axes/LinearAxisExamples.cs
@@ -0,0 +1,18 @@
+﻿namespace ExampleGenerator
+{
+    using OxyPlot;
+    using OxyPlot.Axes;
+
+    public class LinearAxisExamples
+    {
+        [Export(@"Axes\LinearAxis")]
+        public static PlotModel LinearAxis()
+        {
+            var model = new PlotModel { Title = "LinearAxis" };
+
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "Bottom axis" });
+            model.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Left axis" });
+            return model;
+        }
+    }
+}
