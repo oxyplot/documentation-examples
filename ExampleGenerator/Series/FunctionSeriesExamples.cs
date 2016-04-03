4ef601c357103630915a8a0060232a8758532af2
Markus Ebner <hiwatari.seiji@gmail.com>
2016-04-03 22:41:52 +0200

    [FunctionSeries] Add example

---
+++ ExampleGenerator/Series/FunctionSeriesExamples.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExampleGenerator
+{
+	using OxyPlot;
+	using OxyPlot.Axes;
+	using OxyPlot.Series;
+
+	public class FunctionSeriesExamples
+	{
+		[Export(@"Series\FunctionSeries")]
+		public static PlotModel FunctionSeries()
+		{
+			var model = new PlotModel{ Title = "Fun with Bats" };
+
+			Func<double, double> batFn1 = (x) => 2 * Math.Sqrt(-Math.Abs(Math.Abs(x) - 1) * Math.Abs(3 - Math.Abs(x)) / ((Math.Abs(x) - 1) * (3 - Math.Abs(x)))) * (1 + Math.Abs(Math.Abs(x) - 3) / (Math.Abs(x) - 3)) * Math.Sqrt(1 - Math.Pow((x / 7), 2)) + (5 + 0.97 * (Math.Abs(x - 0.5) + Math.Abs(x + 0.5)) - 3 * (Math.Abs(x - 0.75) + Math.Abs(x + 0.75))) * (1 + Math.Abs(1 - Math.Abs(x)) / (1 - Math.Abs(x)));
+			Func<double, double> batFn2 = (x) => -3 * Math.Sqrt(1 - Math.Pow((x / 7), 2)) * Math.Sqrt(Math.Abs(Math.Abs(x) - 4) / (Math.Abs(x) - 4));
+			Func<double, double> batFn3 = (x) => Math.Abs(x / 2) - 0.0913722 * (Math.Pow(x, 2)) - 3 + Math.Sqrt(1 - Math.Pow((Math.Abs(Math.Abs(x) - 2) - 1), 2));
+			Func<double, double> batFn4 = (x) => (2.71052 + (1.5 - .5 * Math.Abs(x)) - 1.35526 * Math.Sqrt(4 - Math.Pow((Math.Abs(x) - 1), 2))) * Math.Sqrt(Math.Abs(Math.Abs(x) - 1) / (Math.Abs(x) - 1)) + 0.9;
+
+			model.Series.Add(new FunctionSeries(batFn1, -8, 8, 0.0001));
+			model.Series.Add(new FunctionSeries(batFn2, -8, 8, 0.0001));
+			model.Series.Add(new FunctionSeries(batFn3, -8, 8, 0.0001));
+			model.Series.Add(new FunctionSeries(batFn4, -8, 8, 0.0001));
+
+			model.Axes.Add(new LinearAxis{ Position = AxisPosition.Bottom, MaximumPadding = 0.1, MinimumPadding = 0.1 });
+			model.Axes.Add(new LinearAxis{ Position = AxisPosition.Left, MaximumPadding = 0.1, MinimumPadding = 0.1 });
+
+			return model;
+		}
+	}
+}
