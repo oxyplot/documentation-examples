dd86928e2a1fc3571c40e88e1fddf0b3dfa6dfbc
Oleg Bogdanov <boguscoder@users.noreply.github.com>
2016-06-14 23:35:12 -0700

    Fixed Android demo app to show basic plot (#6)
    * Fixed Android demo app to show basic plot
    
    * Updated packages to latest version
    
    * moving to latest packages available on Nuget.org
    
    * fixing oxyplot references
    
    * addressing analyzers warnings

--- HelloWorld/AndroidApp1/MainActivity.cs
+++ HelloWorld/AndroidApp1/MainActivity.cs
@@ -1,31 +1,53 @@
-﻿using System;
-using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
+﻿using Android.App;
 using Android.OS;
+
+using OxyPlot.Xamarin.Android;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
 
 namespace AndroidApp1
 {
     [Activity(Label = "AndroidApp1", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
-
-        protected override void OnCreate(Bundle bundle)
+        protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.MyButton);
-
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+			PlotView view = FindViewById<PlotView>(Resource.Id.plot_view);
+			view.Model = CreatePlotModel();
         }
+
+		private PlotModel CreatePlotModel()
+		{
+			var plotModel = new PlotModel { Title = "OxyPlot Demo" };
+
+			plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom });
+			plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Maximum = 10, Minimum = 0 });
+
+			var series1 = new LineSeries
+			{
+				MarkerType = MarkerType.Circle,
+				MarkerSize = 4,
+				MarkerStroke = OxyColors.White
+			};
+
+			series1.Points.Add(new DataPoint(0.0, 6.0));
+			series1.Points.Add(new DataPoint(1.4, 2.1));
+			series1.Points.Add(new DataPoint(2.0, 4.2));
+			series1.Points.Add(new DataPoint(3.3, 2.3));
+			series1.Points.Add(new DataPoint(4.7, 7.4));
+			series1.Points.Add(new DataPoint(6.0, 6.2));
+			series1.Points.Add(new DataPoint(8.9, 8.9));
+
+			plotModel.Series.Add(series1);
+
+			return plotModel;
+		}
     }
 }
 
