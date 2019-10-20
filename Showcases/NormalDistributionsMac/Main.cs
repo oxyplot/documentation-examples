25f884236bd54a0925ac6bea4c354b2644ae2c6b
Oystein Bjorke <oystein.bjorke@gmail.com>
2019-10-20 20:55:49 +0200

    Add Mac example

---
+++ Showcases/NormalDistributionsMac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace NormalDistributionsMac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
---
+++ Showcases/NormalDistributionsMac/ViewController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+using AppKit;
+using CoreGraphics;
+using Foundation;
+
+using OxyPlot.Xamarin.Mac;
+
+using NormalDistributions.Shared;
+
+namespace NormalDistributionsMac
+{
+    public partial class ViewController : NSViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            var frame = new CGRect(10, 10, 600, 400);
+            var plotView = new PlotView(frame);
+            plotView.Model = NormalDistributionModel.Create();
+
+            this.View = plotView;
+        }
+
+        public override NSObject RepresentedObject
+        {
+            get
+            {
+                return base.RepresentedObject;
+            }
+            set
+            {
+                base.RepresentedObject = value;
+                // Update the view, if already loaded.
+            }
+        }
+    }
+}
