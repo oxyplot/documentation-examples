6bef43ba9d0e85e161299bdb8db1c50b11ee8b30
Murdo R Ergeaux <visual.melon@gmail.com>
2019-10-21 00:41:34 +0100

    Add AreaSeries example

---
+++ ExampleGenerator/Series/AreaSeriesExamples.cs
@@ -0,0 +1,34 @@
+﻿namespace ExampleGenerator
+{
+    using System;
+
+    using OxyPlot;
+    using OxyPlot.Axes;
+    using OxyPlot.Series;
+
+    public class AreaSeriesExamples
+    {
+        [Export("Series/AreaSeries")]
+        public static PlotModel AreaSeries()
+        {
+            var model = new PlotModel { Title = "AreaSeries" };
+            model.Axes.Add(new LinearAxis() { Title = "Day", Position = AxisPosition.Bottom });
+            model.Axes.Add(new LinearAxis() { Title = "Output", Position = AxisPosition.Left });
+
+            var areaSeries = new AreaSeries() { Color2 = OxyColors.Transparent };
+
+            var r = new Random(1);
+            var dailyOutput = 10;
+            for (int i = 0; i <= 100; i++)
+            {
+                areaSeries.Points.Add(new DataPoint(i, dailyOutput));
+                //areaSeries.Points2.Add(new DataPoint(i, 0));
+
+                dailyOutput = Math.Max(0, dailyOutput + r.Next(-2, 3));
+            }
+
+            model.Series.Add(areaSeries);
+            return model;
+        }
+    }
+}
